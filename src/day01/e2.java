31dbd9b5e2a67086e9ea612dbf1fe2aa1d84bd1f
Emsal Efe <efe.emsalsiz@gmail.com>
2022-08-13 22:03:31 +0300

    V2

--- src/day01/e2.java
+++ src/day01/e2.java
@@ -4,5 +4,9 @@ public class e2 {
     public static void main(String[] args) {
         System.out.println("3 ve 5 in toplamı :"+ 3+5);
         System.out.println("5 ve 3 un toplamı :"+ 5+3);
+        int sayi =5;
+        int sayi1=3;
+        System.out.println("sayiların toplamı :"+ (sayi+sayi1));
+
     }
 }
