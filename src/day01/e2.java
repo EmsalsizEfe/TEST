89745655de6c55f612aa7c94f84890005bad6cea
Emsal Efe <efe.emsalsiz@gmail.com>
2022-08-13 22:00:17 +0300

    V2

--- src/day01/e2.java
+++ src/day01/e2.java
@@ -3,5 +3,6 @@ package day01;
 public class e2 {
     public static void main(String[] args) {
         System.out.println("3 ve 5 in toplamı :"+ 3+5);
+        System.out.println("5 ve 3 un toplamı :"+ 5+3);
     }
 }
